b666b68ec913b93eb0090d1d6f1cb64f26276fd9
Alexander Pochaev <a.pochaev@gmail.com>
2022-04-26 08:58:04 -0700

    WIP todo items in a form of a test

---
+++ src/test/java/us/pochaev/NextTest.java
@@ -0,0 +1,17 @@
+package us.pochaev;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class NextTest {
+
+	@Test
+	public void next() {
+		fail("Fix tests");
+		fail("Clean up");
+		fail("Attribute parser");
+		fail("Relations");
+		fail("MetaItems if HasMetaItems is implements or Meta Item annotation");
+	}
+}
