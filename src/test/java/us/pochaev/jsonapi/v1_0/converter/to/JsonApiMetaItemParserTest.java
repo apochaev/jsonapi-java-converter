3939e164f3dbc2831780aaffc63c5d31c50dd70e
Alexander Pochaev <a.pochaev@gmail.com>
2022-05-03 08:46:12 -0700

    JsonApiMetaItemParser done done

--- src/test/java/us/pochaev/jsonapi/v1_0/converter/to/JsonApiMetaItemParserTest.java
+++ src/test/java/us/pochaev/jsonapi/v1_0/converter/to/JsonApiMetaItemParserTest.java
@@ -1,15 +1,20 @@
 package us.pochaev.jsonapi.v1_0.converter.to;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static us.pochaev.jsonapi.v1_0.converter.to.test_classes_meta.All.META_ITEM_ANNOTATED_RENAMED_VALUE;
+import static us.pochaev.jsonapi.v1_0.converter.to.test_classes_meta.All.META_ITEM_ANNOTATED_VALUE;
+import static us.pochaev.jsonapi.v1_0.converter.to.test_classes_meta.All.META_ITEM_GETTER_RENAMED_VALUE;
+import static us.pochaev.jsonapi.v1_0.converter.to.test_classes_meta.All.META_ITEM_GETTER_VALUE;
 
 import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import us.pochaev.jsonapi.v1_0.converter.to.test_classes_meta.All;
 import us.pochaev.jsonapi.v1_0.converter.to.test_classes_meta.None;
-import us.pochaev.jsonapi.v1_0.converter.to.test_classes_meta.PublicField;
 
 
 
@@ -23,15 +28,19 @@ public class JsonApiMetaItemParserTest {
 		assertTrue(metaItems.isEmpty());
 	}
 
-	@Test @DisplayName("WHEN public field THEN meta item")
+	@Test @DisplayName("WHEN annotated values THEN meta items")
 	public void whenPublicFieldThenAttribute() {
-		PublicField obj = new PublicField();
-		obj.metaItem1 = "metaItem1";
-		obj.metaItem2 = "metaItem2";
-
+		All obj = new All();
 		Map<String, Object> metaItems = JsonApiMetaItemParser.parse(obj);
-		assertEquals(1, metaItems.size());
-		assertEquals("metaItem1", metaItems.get("metaItem1"));
+		assertEquals(4, metaItems.size());
+		assertEquals(META_ITEM_ANNOTATED_VALUE, metaItems.get("metaItemAnnotated"));
+		assertFalse(metaItems.containsKey("notAnnotated"));
+		assertFalse(metaItems.containsKey("metaItemAnnotatedIgnored"));
+		assertEquals(META_ITEM_ANNOTATED_RENAMED_VALUE, metaItems.get("metaItemAnnotatedRenamed"));
+		assertFalse(metaItems.containsKey("someProperty"));
+		assertEquals(META_ITEM_GETTER_VALUE, metaItems.get("metaItemGetter"));
+		assertEquals(META_ITEM_GETTER_RENAMED_VALUE, metaItems.get("metaItemGetterRenamed"));
+		assertFalse(metaItems.containsKey("metaItemGetterIgnored"));
 	}
 
 
--- src/test/java/us/pochaev/jsonapi/v1_0/converter/to/test_classes_meta/All.java
+++ src/test/java/us/pochaev/jsonapi/v1_0/converter/to/test_classes_meta/All.java
@@ -1,17 +1,53 @@
 package us.pochaev.jsonapi.v1_0.converter.to.test_classes_meta;
 
 import us.pochaev.jsonapi.v1_0.annotations.JsonApiId;
+import us.pochaev.jsonapi.v1_0.annotations.JsonApiIgnore;
 import us.pochaev.jsonapi.v1_0.annotations.JsonApiMetaItem;
 import us.pochaev.jsonapi.v1_0.annotations.JsonApiObject;
 
 @JsonApiObject
-public class PublicField {
+public class All {
+	public static final Object ID = null;
+	public static final Object META_ITEM_ANNOTATED_VALUE = "metaItemAnnotated_value";
+	public static final Object META_ITEM_ANNOTATED_RENAMED_VALUE = "metaItemAnnotatedRenamed_value";
+	public static final Object META_ITEM_GETTER_VALUE = "metaItemGetter_value";
+	public static final Object META_ITEM_GETTER_RENAMED_VALUE = "metaItemGetterRenamed_value";
+
 	@JsonApiId
-	public Object id;
+	public Object id = ID;
 
 	@JsonApiMetaItem
-	public Object metaItem1;
+	public Object metaItemAnnotated = META_ITEM_ANNOTATED_VALUE;
 
-	public Object metaItem2;
+	public Object notAnnotated = null;
+
+	@JsonApiMetaItem
+	@JsonApiIgnore
+	public Object metaItemAnnotatedIgnored = null;
+
+	@JsonApiMetaItem("metaItemAnnotatedRenamed")
+	public Object metaItemAnnotatedRenamed = META_ITEM_ANNOTATED_RENAMED_VALUE;
+
+	public Object getSomeProperty() {
+		return null;
+	}
+
+	@JsonApiMetaItem
+	public Object getMetaItemGetter() {
+		return META_ITEM_GETTER_VALUE;
+	}
+
+	@JsonApiMetaItem("metaItemGetterRenamed")
+	public Object getMetaItemGetterRenamed() {
+		return META_ITEM_GETTER_RENAMED_VALUE;
+	}
+
+	@JsonApiMetaItem
+	@JsonApiIgnore
+	public Object getMetaItemGetterIgnored() {
+		return null;
+	}
+
 
 }
+
